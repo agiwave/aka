ab96b4d16db1e32500948f1af90d45d02c33a43c
agiwave <sanbgi@qq.com>
2024-04-04 21:35:24 +0800

    Remove HF token

--- examples/vision/MNIST.py
+++ examples/vision/MNIST.py
@@ -23,7 +23,7 @@ def MNIST(num_channels=1, num_classes=10):
 if __name__ == "__main__":
     import aka.numpy as np
     import aka.repo as repo
-    datasets = repo.AutoDataset('mnist', token='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')
+    datasets = repo.AutoDataset('mnist')
     def collate_fn(items):
         inputs = np.stack([np.array(item['image'], dtype=np.float) / 255 for item in items], dim=0)
         targets = np.array([item['label'] for item in items])
