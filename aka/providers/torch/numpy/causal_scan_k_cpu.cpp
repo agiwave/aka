9d5deaa015f54a66a83553671042cc8557f34044
billwu <sanbgi@qq.com>
2024-04-13 12:54:31 +0800

    Add struct shape_t in causalscan cuda kernel.

--- aka/providers/torch/numpy/causal_scan_k_cpu.cpp
+++ aka/providers/torch/numpy/causal_scan_k_cpu.cpp
@@ -5,11 +5,18 @@ typedef struct{
     int x, y, z;
 }INDICS;
 
+typedef struct{
+    int b;
+    int l;
+    int h;
+    int d;
+}shape_t;
+
 #define atomicAdd(p,b) ((*(p)) += (b))
 
 namespace { namespace device {
     template <typename scalar_t> void causalScan4d_cpu_Forward(
-        int b, int l, int h, int d,
+        const shape_t shapeA,
         scalar_t * Z,
         scalar_t * A,
         scalar_t * B,
@@ -18,13 +25,14 @@ namespace { namespace device {
         INDICS& gridDim
     )
     {
-        int idx_A = ((blockIdx.x % b) * l * h + blockIdx.y) * d + blockIdx.z % d;
-        int idx_B = (blockIdx.x * l * gridDim.y + blockIdx.y) * gridDim.z + blockIdx.z;
+        int idx_A = ((blockIdx.x % shapeA.b) * shapeA.l * shapeA.h + blockIdx.y) * shapeA.d + blockIdx.z % shapeA.d;
+        int idx_B = (blockIdx.x * shapeA.l * gridDim.y + blockIdx.y) * gridDim.z + blockIdx.z;
         int idx_Z = (blockIdx.x * gridDim.y + blockIdx.y) * gridDim.z + blockIdx.z;
-        int step_A = h * d;
+        int step_A = shapeA.h * shapeA.d;
         int step_B = gridDim.y * gridDim.z;
         scalar_t zh = Z[idx_Z];
-        while(l-->0) {
+        int length = shapeA.l;
+        while(length-->0) {
             zh = A[idx_A] * zh + B[idx_B];
             O[idx_B] = zh;
             idx_A += step_A;
@@ -33,7 +41,7 @@ namespace { namespace device {
     }
 
     template <typename scalar_t> void causalScan4d_cpu_Backward(
-        int b, int l, int h, int d,
+        const shape_t shapeA,
         scalar_t * gradZ,
         scalar_t * gradA,
         scalar_t * gradB,
@@ -45,15 +53,16 @@ namespace { namespace device {
         INDICS& gridDim
     )
     {
-        int idx_A = ((blockIdx.x % b) * l * h + blockIdx.y) * d + blockIdx.z % d;
-        int idx_B = (blockIdx.x * l * gridDim.y + blockIdx.y) * gridDim.z + blockIdx.z;
+        int idx_A = ((blockIdx.x % shapeA.b) * shapeA.l * shapeA.h + blockIdx.y) * shapeA.d + blockIdx.z % shapeA.d;
+        int idx_B = (blockIdx.x * shapeA.l * gridDim.y + blockIdx.y) * gridDim.z + blockIdx.z;
         int idx_Z = (blockIdx.x * gridDim.y + blockIdx.y) * gridDim.z + blockIdx.z;
-        int step_A = h * d;
+        int step_A = shapeA.h * shapeA.d;
         int step_B = gridDim.y * gridDim.z;
-        idx_A += step_A * (l - 1);
-        idx_B += step_B * (l - 1);
+        int length = shapeA.l;
+        idx_A += step_A * (length - 1);
+        idx_B += step_B * (length - 1);
         scalar_t grad = 0.0;
-        while(l-->1) {
+        while(length-->1) {
             grad += gradO[idx_B];
             gradB[idx_B] = grad;
             atomicAdd(gradA + idx_A, O[idx_B - step_B] * grad);
@@ -70,10 +79,7 @@ namespace { namespace device {
 
 torch::Tensor causalScan4d_cpu_Forward(torch::Tensor Z, torch::Tensor A, torch::Tensor B) {
     auto O = torch::zeros_like(B);
-    int ba = A.size(0);
-    int la = A.size(1);
-    int ha = A.size(2);
-    int da = A.size(3);
+    shape_t shapeA = {(int)A.size(0), (int)A.size(1), (int)A.size(2), (int)A.size(3)};
 
     int b = O.size(0);
     int l = O.size(1);
@@ -88,7 +94,7 @@ torch::Tensor causalScan4d_cpu_Forward(torch::Tensor Z, torch::Tensor A, torch::
         };
         AT_DISPATCH_FLOATING_TYPES(O.type(), "causalScan4d_Backward", ([&] {
             device::causalScan4d_cpu_Forward<scalar_t>(
-                ba, la, ha, da,
+                shapeA,
                 (scalar_t*)Z.data_ptr(),
                 (scalar_t*)A.data_ptr(),
                 (scalar_t*)B.data_ptr(),
@@ -106,11 +112,8 @@ std::vector<torch::Tensor> causalScan4d_cpu_Backward(torch::Tensor gradO, torch:
     auto gradA = torch::zeros_like(A);
     auto gradB = torch::zeros_like(O);
     auto gradZ = torch::zeros_like(O.slice(1, 0, 1));
-    int ba = A.size(0);
-    int la = A.size(1);
-    int ha = A.size(2);
-    int da = A.size(3);
-    
+    shape_t shapeA = {(int)A.size(0), (int)A.size(1), (int)A.size(2), (int)A.size(3)};
+
     int b = O.size(0);
     int l = O.size(1);
     int h = O.size(2);
@@ -124,7 +127,7 @@ std::vector<torch::Tensor> causalScan4d_cpu_Backward(torch::Tensor gradO, torch:
         };
         AT_DISPATCH_FLOATING_TYPES(O.type(), "causalScan4d_Backward", ([&] {
             device::causalScan4d_cpu_Backward(
-                ba, la, ha, da,
+                shapeA,
                 (scalar_t*)gradZ.data_ptr(),
                 (scalar_t*)gradA.data_ptr(),
                 (scalar_t*)gradB.data_ptr(),
--- aka/providers/torch/numpy/causal_scan_k_cuda.cu
+++ aka/providers/torch/numpy/causal_scan_k_cuda.cu
@@ -1,22 +1,30 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+typedef struct{
+    int b;
+    int l;
+    int h;
+    int d;
+}shape_t;
+
 namespace { namespace device {
     template <typename scalar_t> __global__ void causalScan4d_Forward(
-        int b, int l, int h, int d,
+        const shape_t shapeA,
         scalar_t * Z,
         scalar_t * A,
         scalar_t * B,
         scalar_t * O
     )
     {
-        int idx_A = ((blockIdx.x % b) * l * h + blockIdx.y) * d + blockIdx.z % d;
-        int idx_B = (blockIdx.x * l * gridDim.y + blockIdx.y) * gridDim.z + blockIdx.z;
+        int idx_A = ((blockIdx.x % shapeA.b) * shapeA.l * shapeA.h + blockIdx.y) * shapeA.d + blockIdx.z % shapeA.d;
+        int idx_B = (blockIdx.x * shapeA.l * gridDim.y + blockIdx.y) * gridDim.z + blockIdx.z;
         int idx_Z = (blockIdx.x * gridDim.y + blockIdx.y) * gridDim.z + blockIdx.z;
-        int step_A = h * d;
+        int step_A = shapeA.h * shapeA.d;
         int step_B = gridDim.y * gridDim.z;
         scalar_t zh = Z[idx_Z];
-        while(l-->0) {
+        int length = shapeA.l;
+        while(length-->0) {
             zh = A[idx_A] * zh + B[idx_B];
             O[idx_B] = zh;
             idx_A += step_A;
@@ -25,7 +33,7 @@ namespace { namespace device {
     }
 
     template <typename scalar_t> __global__ void causalScan4d_Backward(
-        int b, int l, int h, int d,
+        const shape_t shapeA,
         scalar_t * gradZ,
         scalar_t * gradA,
         scalar_t * gradB,
@@ -35,15 +43,16 @@ namespace { namespace device {
         scalar_t * O
     )
     {
-        int idx_A = ((blockIdx.x % b) * l * h + blockIdx.y) * d + blockIdx.z % d;
-        int idx_B = (blockIdx.x * l * gridDim.y + blockIdx.y) * gridDim.z + blockIdx.z;
+        int idx_A = ((blockIdx.x % shapeA.b) * shapeA.l * shapeA.h + blockIdx.y) * shapeA.d + blockIdx.z % shapeA.d;
+        int idx_B = (blockIdx.x * shapeA.l * gridDim.y + blockIdx.y) * gridDim.z + blockIdx.z;
         int idx_Z = (blockIdx.x * gridDim.y + blockIdx.y) * gridDim.z + blockIdx.z;
-        int step_A = h * d;
+        int step_A = shapeA.h * shapeA.d;
         int step_B = gridDim.y * gridDim.z;
-        idx_A += step_A * (l - 1);
-        idx_B += step_B * (l - 1);
+        int length = shapeA.l;
+        idx_A += step_A * (length - 1);
+        idx_B += step_B * (length - 1);
         scalar_t grad = 0.0;
-        while(l-->1) {
+        while(length-->1) {
             grad += gradO[idx_B];
             gradB[idx_B] = grad;
             atomicAdd(gradA + idx_A, O[idx_B - step_B] * grad);
@@ -67,14 +76,11 @@ torch::Tensor causalScan4d_cuda_Forward(torch::Tensor Z, torch::Tensor A, torch:
         return causalScan4d_cpu_Forward(Z,A,B);
     }
     auto O = torch::zeros_like(B);
-    int ba = A.size(0);
-    int la = A.size(1);
-    int ha = A.size(2);
-    int da = A.size(3);
+    shape_t shapeA = {(int)A.size(0), (int)A.size(1), (int)A.size(2), (int)A.size(3)};
     const dim3 blocks(O.size(0), O.size(2), O.size(3));
     AT_DISPATCH_FLOATING_TYPES(O.type(), "causalScan4d_Forward", ([&] {
         device::causalScan4d_Forward<scalar_t><<<blocks, 1>>>(
-            ba, la, ha, da,
+            shapeA,
             (scalar_t*)Z.data_ptr(),
             (scalar_t*)A.data_ptr(),
             (scalar_t*)B.data_ptr(),
@@ -92,14 +98,11 @@ std::vector<torch::Tensor> causalScan4d_cuda_Backward(torch::Tensor gradO, torch
     auto gradA = torch::zeros_like(A);
     auto gradB = torch::zeros_like(O);
     auto gradZ = torch::zeros_like(O.slice(1, 0, 1));
-    int ba = A.size(0);
-    int la = A.size(1);
-    int ha = A.size(2);
-    int da = A.size(3);
+    shape_t shapeA = {(int)A.size(0), (int)A.size(1), (int)A.size(2), (int)A.size(3)};
     const dim3 blocks(O.size(0), O.size(2), O.size(3));
     AT_DISPATCH_FLOATING_TYPES(O.type(), "causalScan4d_Backward", ([&] {
         device::causalScan4d_Backward<scalar_t><<<blocks, 1>>>(
-            ba, la, ha, da,
+            shapeA,
             (scalar_t*)gradZ.data_ptr(),
             (scalar_t*)gradA.data_ptr(),
             (scalar_t*)gradB.data_ptr(),
